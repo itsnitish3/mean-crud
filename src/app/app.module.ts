65834e502bc9cde23d6bcce73012327f3b62c299
Nitish <nitish.gupta@vivainfomedia.com>
2021-04-27 12:43:40 +0530

    component form and added added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
