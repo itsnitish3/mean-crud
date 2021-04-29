d7474b3253f05884e80d9e5ea6471f6cb3765730
Nitish <nitish.gupta@vivainfomedia.com>
2021-04-29 10:19:20 +0530

    form controll done

--- src/app/post-form/post-form.component.ts
+++ src/app/post-form/post-form.component.ts
@@ -1,15 +1,67 @@
-import { Component, OnInit } from '@angular/core';
-
+import {Component,OnInit} from '@angular/core';
+import {FormBuilder, FormControl, Validators} from '@angular/forms';
+import { User } from '../user'
+import { UserService } from '../user.service';
+/** @title Form field with error messages */
 @Component({
   selector: 'app-post-form',
   templateUrl: './post-form.component.html',
   styleUrls: ['./post-form.component.css']
 })
 export class PostFormComponent implements OnInit {
+  email = new FormControl('', [Validators.required, Validators.email]);
+  phone = new FormControl('',[Validators.required, Validators.minLength(10), Validators.pattern('^[0-9]*$')]);
+  name = new FormControl('',[Validators.required,  Validators.minLength(3), Validators.pattern('^[a-z,A-Z]*$')]);
+  signupForm = this.formBuilder.group({
+    name: this.name,
+    phone:this.phone,
+    email:this.email
+  });
+  constructor(
+    private formBuilder: FormBuilder,private _UserService: UserService
+    ) {}
+    onSubmit():void{
+      console.log(this.signupForm.value)
+      var body= JSON.stringify(this.signupForm.value);
+      console.log(body)
 
-  constructor() { }
+    }
+    listuser:User[];
 
-  ngOnInit(): void {
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+  ngOnInit() {
+    this._UserService.getuser().subscribe(
+      data=>{
+            this.listuser= data;
+            // console.log(this.listuser)
+      }
+    );
+
   }
 
+  submitData(){
+    if(this.phone.value || this.email.value || this.phone.value){
+      let body: any = {
+        name: this.name.value,
+        email: this.email.value,
+        phone: this.phone.value
+      };
+
+      this._UserService.postUser(body).subscribe(data =>{
+        console.log(data);
+      }, err=>{
+        console.log('err', err)
+      });
+    }else{
+      console.log('Please add data properly')
+    }
+
+
+  }
 }
+
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams} from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class UserService {
@@ -8,10 +9,31 @@ export class UserService {
   constructor(private httpclient:HttpClient) { }
 
   getuser(): Observable<any>{
-return this.httpclient.get("http://localhost:4300/api/get")
+return this.httpclient.get(environment.apiUrl+"/api/get")
   }
   getuseid(): Observable<any>{
 let param1= new HttpParams().set('id','6087ef92fd786b7a4631af50');
-return this.httpclient.get("http://localhost:4300/api/get/id",{params:param1})
+return this.httpclient.get(environment.apiUrl+"/api/get/id",{params:param1})
   }
+  // postuser(): Observable<any>{
+  //   this.httpclient.post<any>('http://localhost:4300/api/create', { }).subscribe(data => {
+  //           // this.postId = data.id;
+  //           return this.data = data;
+  //       })
+  // }
+  // postuser():Observable{
+  //  this.httpclient.post('http://localhost:4300/api/create',body).subscribe(
+  //     response=>{
+  //       console.log(response)
+  //     return this.User = response
+  //     },err=>{
+  //       console.log(err)
+  //     }
+  //   )
+  // }
+
+  postUser(body:any) : Observable<any>{
+    return this.httpclient.post(environment.apiUrl+'/api/create', body);
+  }
+
 }
